af0103e3c621008b85fb632150c3c32d1167194a
ash15-pro <aishwaryabehare2@gmail.com>
2026-04-01 21:45:36 +0530

    added home endpoint

--- src/main/java/com/email/writer/EmailGeneratorController.java
+++ src/main/java/com/email/writer/EmailGeneratorController.java
@@ -2,10 +2,7 @@ package com.email.writer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
@@ -16,5 +13,9 @@ public class EmailGeneratorController {
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
         String response=emailGeneratorService.generateEmailReply(emailRequest);
         return ResponseEntity.ok(response);
+    }
+    @GetMapping("/")
+    public String home() {
+        return "AI Email Writer API is running 🚀";
     }
 }
